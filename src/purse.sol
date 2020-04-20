1a9ac4fb798c4554d235f944134216a08dd0f3f1
stefanionescu <codrinionescu@yahoo.com>
2020-04-20 02:18:02 +0100

    purse returns early in case of any problem with pulling

--- src/purse.sol
+++ src/purse.sol
@@ -1,3 +1,20 @@
+/// purse.sol -- stability fee treasury
+
+// Copyright (C) 2020 Stefan C. Ionescu <stefanionescu@protonmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 pragma solidity ^0.5.15;
 
 import "./lib.sol";
@@ -5,6 +22,16 @@ import "./lib.sol";
 contract VatLike {
     function move(address,address,uint) external;
     function good(address) external view returns (uint);
+}
+contract GemLike {
+    function balanceOf(address) external view returns (uint);
+    function transfer(address,uint) external returns (bool);
+    function transferFrom(address,address,uint) external returns (bool);
+}
+contract CoinJoinLike {
+    function coin() external view returns (address);
+    function join(address, uint) external;
+    function exit(address, uint) external;
 }
 
 contract Purse is LibNote {
@@ -18,20 +45,22 @@ contract Purse is LibNote {
     }
 
     mapping(address => uint) public allowance;
-    mapping(address => uint) public pace;
 
-    VatLike public vat;
+    VatLike      public vat;
+    GemLike      public coin;
+    CoinJoinLike public coinJoin;
 
     address public vow;
 
-    uint8   public mutex;
-
     uint256 public live;
 
-    constructor(address vat_, address vow_) public {
+    constructor(address vat_, address vow_, address coinJoin_) public {
         wards[msg.sender] = 1;
-        vat = VatLike(vat_);
-        vow = vow_;
+        vat  = VatLike(vat_);
+        vow  = vow_;
+        coinJoin = CoinJoinLike(coinJoin_);
+        coin = GemLike(coinJoin.coin());
+        require(address(coin) != address(0), "Purse/null-coin");
         live = 1;
     }
 
@@ -68,14 +97,15 @@ contract Purse is LibNote {
         live = 0;
     }
 
+    // --- Utils ---
+    function either(bool x, bool y) internal pure returns (bool z) {
+        assembly{ z := or(x, y)}
+    }
+
     // --- Allowance ---
     function allow(address gal, uint val) external note auth {
         require(gal != address(0), "Purse/null-gal");
         allowance[gal] = val;
-    }
-    function limit(address gal, uint val) external note auth {
-        require(gal != address(0), "Purse/null-gal");
-        pace[gal] = val;
     }
 
     // --- Stability Fee Transfer (Governance) ---
@@ -88,15 +118,16 @@ contract Purse is LibNote {
     }
 
     // --- Stability Fee Transfer (Approved Gals) ---
-    function pull(address gal, uint val) external returns (bool) {
-        require(mutex == 0, "Purse/non-null-mutex");
-        mutex = 1;
-        if (pace[msg.sender] > 0) {
-          require(val <= pace[msg.sender], "Purse/exceeds-pace");
+    function pull(address gal, address tkn, uint val) external returns (bool) {
+        if (either(either(allowance[msg.sender] < val, either(gal == address(0), val == 0)), tkn != address(coin))) {
+          return false;
         }
         allowance[msg.sender] = sub(allowance[msg.sender], val);
-        vat.move(address(this), gal, val);
-        mutex = 0;
+        uint exit = (coin.balanceOf(address(this)) >= val) ? 0 : sub(val, coin.balanceOf(address(this)));
+        if (exit > 0) {
+          coinJoin.exit(address(this), exit);
+        }
+        coin.transfer(gal, val);
         return true;
     }
 }
