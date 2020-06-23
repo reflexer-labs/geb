5f48aabc3ce0b519a91ad4964b9f0e0731adb6e7
stefanionescu <stefanionescu@protonmail.com>
2020-06-23 11:47:38 +0100

    Correct copyright

--- src/SurplusAuctionHouse.sol
+++ src/SurplusAuctionHouse.sol
@@ -1,7 +1,6 @@
 /// SurplusAuctionHouse.sol
 
 // Copyright (C) 2018 Rain <rainbreak@riseup.net>
-// Copyright (C) 2020 Stefan C. Ionescu <stefanionescu@protonmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
