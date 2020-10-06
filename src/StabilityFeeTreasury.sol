0884a757b4b1f2bbe485a835cbef4ad0a73d366e
stefanionescu <stefanionescu@protonmail.com>
2020-10-06 02:04:21 +0100

    Fix copyright

--- src/StabilityFeeTreasury.sol
+++ src/StabilityFeeTreasury.sol
@@ -1,6 +1,6 @@
 /// StabilityFeeTreasury.sol
 
-// Copyright (C) 2020 Reflexer Labs, INC
+// Copyright (C) 2018 Rain <rainbreak@riseup.net>, 2020 Reflexer Labs, INC
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
