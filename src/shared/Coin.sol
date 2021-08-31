4bb25e45595b267131ae88356b284d8df9363d4b
Stefan Ionescu <stefan_alter_ego@protonmail.com>
2021-08-31 23:55:38 +0100

    Make optimism coin

--- src/shared/Coin.sol
+++ src/shared/Coin.sol
@@ -231,3 +231,242 @@ contract Coin {
         emit Approval(holder, spender, wad);
     }
 }
+
+contract OptimismCoin {
+    // --- Auth ---
+    mapping (address => uint256) public authorizedAccounts;
+    /**
+     * @notice Add auth to an account
+     * @param account Account to add auth to
+     */
+    function addAuthorization(address account) external isAuthorized {
+        require(msg.sender != l2Bridge, "Coin/bridge-cannot-deauth");
+
+        authorizedAccounts[account] = 1;
+        emit AddAuthorization(account);
+    }
+    /**
+     * @notice Remove auth from an account
+     * @param account Account to remove auth from
+     */
+    function removeAuthorization(address account) external isAuthorized {
+        require(msg.sender != l2Bridge, "Coin/bridge-cannot-deauth");
+        require(account != l2Bridge, "Coin/cannot-remove-bridge");
+
+        authorizedAccounts[account] = 0;
+        emit RemoveAuthorization(account);
+    }
+    /**
+    * @notice Checks whether msg.sender can call an authed function
+    **/
+    modifier isAuthorized {
+        require(authorizedAccounts[msg.sender] == 1, "Coin/account-not-authorized");
+        _;
+    }
+
+    // --- ERC20 Data ---
+    // The name of this coin
+    string  public name;
+    // The symbol of this coin
+    string  public symbol;
+    // The version of this Coin contract for L2 Optimism
+    string  public version = "2";
+    // The number of decimals that this coin has
+    uint8   public constant decimals = 18;
+
+    // The id of the chain where this coin was deployed
+    uint256 public chainId;
+    // The total supply of this coin
+    uint256 public totalSupply;
+
+    // L1 Coin address
+    address public l1Token;
+    // L2 bridge that's allowed to mint/burn
+    address public l2Bridge;
+
+    // Mapping of coin balances
+    mapping (address => uint256)                      public balanceOf;
+    // Mapping of allowances
+    mapping (address => mapping (address => uint256)) public allowance;
+    // Mapping of nonces used for permits
+    mapping (address => uint256)                      public nonces;
+
+    // --- Events ---
+    event AddAuthorization(address account);
+    event RemoveAuthorization(address account);
+    event Approval(address indexed src, address indexed guy, uint256 amount);
+    event Transfer(address indexed src, address indexed dst, uint256 amount);
+
+    // --- Math ---
+    function addition(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "Coin/add-overflow");
+    }
+    function subtract(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "Coin/sub-underflow");
+    }
+
+    // --- EIP712 niceties ---
+    bytes32 public DOMAIN_SEPARATOR;
+    // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
+    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 chainId_,
+        address l1Token_,
+        address l2Bridge_
+      ) public {
+        require(l1Token_ != address(0), "Coin/null-l1-token");
+        require(l2Bridge_ != address(0), "Coin/null-l2-bridge");
+
+        authorizedAccounts[msg.sender] = 1;
+        authorizedAccounts[l2Bridge_]  = 1;
+
+        name          = name_;
+        symbol        = symbol_;
+        chainId       = chainId_;
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes(name)),
+            keccak256(bytes(version)),
+            chainId_,
+            address(this)
+        ));
+
+        l1Token       = l1Token_;
+        l2Bridge      = l2Bridge_;
+
+        emit AddAuthorization(msg.sender);
+        emit AddAuthorization(l2Bridge);
+    }
+
+    // --- Token ---
+    /*
+    * @notice Transfer coins to another address
+    * @param dst The address to transfer coins to
+    * @param amount The amount of coins to transfer
+    */
+    function transfer(address dst, uint256 amount) external returns (bool) {
+        return transferFrom(msg.sender, dst, amount);
+    }
+    /*
+    * @notice Transfer coins from a source address to a destination address (if allowed)
+    * @param src The address from which to transfer coins
+    * @param dst The address that will receive the coins
+    * @param amount The amount of coins to transfer
+    */
+    function transferFrom(address src, address dst, uint256 amount)
+        public returns (bool)
+    {
+        require(dst != address(0), "Coin/null-dst");
+        require(dst != address(this), "Coin/dst-cannot-be-this-contract");
+        require(balanceOf[src] >= amount, "Coin/insufficient-balance");
+        if (src != msg.sender && allowance[src][msg.sender] != uint256(-1)) {
+            require(allowance[src][msg.sender] >= amount, "Coin/insufficient-allowance");
+            allowance[src][msg.sender] = subtract(allowance[src][msg.sender], amount);
+        }
+        balanceOf[src] = subtract(balanceOf[src], amount);
+        balanceOf[dst] = addition(balanceOf[dst], amount);
+        emit Transfer(src, dst, amount);
+        return true;
+    }
+    /*
+    * @notice Mint new coins
+    * @param usr The address for which to mint coins
+    * @param amount The amount of coins to mint
+    */
+    function mint(address usr, uint256 amount) external isAuthorized {
+        balanceOf[usr] = addition(balanceOf[usr], amount);
+        totalSupply    = addition(totalSupply, amount);
+        emit Transfer(address(0), usr, amount);
+    }
+    /*
+    * @notice Burn coins from an address
+    * @param usr The address that will have its coins burned
+    * @param amount The amount of coins to burn
+    */
+    function burn(address usr, uint256 amount) external {
+        require(balanceOf[usr] >= amount, "Coin/insufficient-balance");
+        if (usr != msg.sender && allowance[usr][msg.sender] != uint256(-1)) {
+            require(allowance[usr][msg.sender] >= amount, "Coin/insufficient-allowance");
+            allowance[usr][msg.sender] = subtract(allowance[usr][msg.sender], amount);
+        }
+        balanceOf[usr] = subtract(balanceOf[usr], amount);
+        totalSupply    = subtract(totalSupply, amount);
+        emit Transfer(usr, address(0), amount);
+    }
+    /*
+    * @notice Change the transfer/burn allowance that another address has on your behalf
+    * @param usr The address whose allowance is changed
+    * @param amount The new total allowance for the usr
+    */
+    function approve(address usr, uint256 amount) external returns (bool) {
+        allowance[msg.sender][usr] = amount;
+        emit Approval(msg.sender, usr, amount);
+        return true;
+    }
+
+    // --- Alias ---
+    /*
+    * @notice Send coins to another address
+    * @param usr The address to send tokens to
+    * @param amount The amount of coins to send
+    */
+    function push(address usr, uint256 amount) external {
+        transferFrom(msg.sender, usr, amount);
+    }
+    /*
+    * @notice Transfer coins from another address to your address
+    * @param usr The address to take coins from
+    * @param amount The amount of coins to take from the usr
+    */
+    function pull(address usr, uint256 amount) external {
+        transferFrom(usr, msg.sender, amount);
+    }
+    /*
+    * @notice Transfer coins from another address to a destination address (if allowed)
+    * @param src The address to transfer coins from
+    * @param dst The address to transfer coins to
+    * @param amount The amount of coins to transfer
+    */
+    function move(address src, address dst, uint256 amount) external {
+        transferFrom(src, dst, amount);
+    }
+
+    // --- Approve by signature ---
+    /*
+    * @notice Submit a signed message that modifies an allowance for a specific address
+    */
+    function permit(
+        address holder,
+        address spender,
+        uint256 nonce,
+        uint256 expiry,
+        bool allowed,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external
+    {
+        bytes32 digest =
+            keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH,
+                                     holder,
+                                     spender,
+                                     nonce,
+                                     expiry,
+                                     allowed))
+        ));
+
+        require(holder != address(0), "Coin/invalid-address-0");
+        require(holder == ecrecover(digest, v, r, s), "Coin/invalid-permit");
+        require(expiry == 0 || now <= expiry, "Coin/permit-expired");
+        require(nonce == nonces[holder]++, "Coin/invalid-nonce");
+        uint256 wad = allowed ? uint256(-1) : 0;
+        allowance[holder][spender] = wad;
+        emit Approval(holder, spender, wad);
+    }
+}
